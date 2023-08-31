5f80d9be076f310241eaf259e0331af9b827742d
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-08-31 12:23:29 -0400

    added skeletons for API calls

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db');
 const connect = require('./dbConnect');
 
 const app = express();
@@ -8,6 +9,52 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 connect();
+
+// API Calls
+// convert date to datetime using cast command
+
+app.get('/qa/questions/:product_id', (req, res) => {
+  const id = req.params.product_id;
+
+})
+
+// app.get('/qa/questions/:question_id/answers', (req, res) => {
+//   // confirm params
+//   const id = req.params.question_id;
+
+// })
+
+// app.post('/qa/questions', (req, res) => {
+//   // confirm data
+//   const data = req.body;
+
+// })
+
+// app.post('/qa/questions/:question_id/answers', (req, res) => {
+//   // confirm params
+//   const id = req.params.question_id;
+
+// })
+
+// app.put('/qa/questions/:question_id/helpful', (req, res) => {
+//   const id = req.params.question_id;
+
+// })
+
+// app.put('/qa/questions/:question_id/report', (req, res) => {
+//   const id = req.params.question_id;
+
+// })
+
+// app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+//   const id = req.params.answer_id;
+
+// })
+
+// app.put('/qa/answers/:answer_id/report', (req, res) => {
+//   const id = req.params.answer_id;
+
+// })
 
 app.listen(process.env.PGPORT, (err) => {
   if (err) {
