59f6e696214c6bf7f20986b7221a8f22d20702b1
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-09-01 07:53:36 -0400

    added three more put requests

--- server/database/dbModels.js
+++ server/database/dbModels.js
@@ -28,7 +28,7 @@ const fetchAns = (req, res) => {
     .catch(err => res.status(500).send('Database connection error'));
 }
 
-const postQue  = (req, res) => {
+const postQue = (req, res) => {
   // const id = req.params.product_id;
   // const data = req.body;
 
@@ -43,9 +43,7 @@ const postQue  = (req, res) => {
   //   .catch(err => res.status(500).send('Database connection error'));
 }
 
-// UPDATE COMPANY SET SALARY = 15000 WHERE ID = 3;
 const helpQue = (req, res) => {
-  // need to retrieve previous amount to then add one to...
   const id = req.params.question_id;
 
   db.connect()
@@ -71,8 +69,91 @@ const helpQue = (req, res) => {
     .catch(err => res.status(500).send('Database connection error'));
 }
 
+const repQue = (req, res) => {
+  const id = req.params.question_id;
+
+  db.connect()
+    .then(client => {
+      client
+        .query('SELECT q_report FROM questions WHERE q_id = $1', [id])
+        .then(result => {
+          if (result.rows.length > 0) {
+            const prev = result.rows[0].q_report;
+            const report = prev + 1;
+
+            client
+              .query('UPDATE questions SET q_report = $1 WHERE q_id = $2', [report, id])
+              .then(() => res.status(204).send('Database updated successfully'))
+              .catch(err => res.status(500).send('Cannot update database'))
+              .finally(() => client.release());
+          } else {
+            res.status(404).send('Question not found');
+          }
+        })
+        .catch(err => res.status(500).send('Cannot query database'))
+    })
+    .catch(err => res.status(500).send('Database connection error'));
+}
+
+const helpAns = (req, res) => {
+  const id = req.params.answer_id;
+
+  db.connect()
+    .then(client => {
+      client
+        .query('SELECT a_helpful FROM answers WHERE a_id = $1', [id])
+        .then(result => {
+          if (result.rows.length > 0) {
+            const prev = result.rows[0].a_helpful;
+            const helpful = prev + 1;
+
+            client
+              .query('UPDATE answers SET a_helpful = $1 WHERE a_id = $2', [helpful, id])
+              .then(() => res.status(204).send('Database updated successfully'))
+              .catch(err => res.status(500).send('Cannot update database'))
+              .finally(() => client.release());
+          } else {
+            res.status(404).send('Question not found');
+          }
+        })
+        .catch(err => res.status(500).send('Cannot query database'))
+    })
+    .catch(err => res.status(500).send('Database connection error'));
+}
+
+const repAns = (req, res) => {
+  const id = req.params.answer_id;
+
+  db.connect()
+    .then(client => {
+      client
+        .query('SELECT a_report FROM answers WHERE a_id = $1', [id])
+        .then(result => {
+          if (result.rows.length > 0) {
+            const prev = result.rows[0].a_report;
+            const report = prev + 1;
+
+            client
+              .query('UPDATE answers SET a_report = $1 WHERE a_id = $2', [report, id])
+              .then(() => res.status(204).send('Database updated successfully'))
+              .catch(err => res.status(500).send('Cannot update database'))
+              .finally(() => client.release());
+          } else {
+            res.status(404).send('Question not found');
+          }
+        })
+        .catch(err => res.status(500).send('Cannot query database'))
+    })
+    .catch(err => res.status(500).send('Database connection error'));
+}
+
 module.exports = {
   fetchQue,
   fetchAns,
-  postQue
+  postQue,
+  postAns,
+  helpQue,
+  repQue,
+  helpAns,
+  repAns
 }
