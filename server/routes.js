2d7421c4872de6da997de387228d2143cf44fecb
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-09-07 10:36:30 -0400

    another one

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const models = require('./database/dbModels');
+const loaderio = require('./loaderio.txt');
 
 router.get('/qa/questions/:product_id', models.fetchQue);
 router.get('/qa/questions/:question_id/answers', models.fetchAns);
@@ -10,5 +11,6 @@ router.put('/qa/questions/:question_id/helpful', models.helpQue);
 router.put('/qa/questions/:question_id/report', models.repQue);
 router.put('/qa/answers/:answer_id/helpful', models.helpAns);
 router.put('/qa/answers/:answer_id/report', models.repAns);
+router.get('/loaderio-e37a58cf89c7c4475f8b6f73d6230d8d.txt', loaderio);
 
 module.exports = router;
